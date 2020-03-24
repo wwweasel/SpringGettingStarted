61932a6b63dd5f7141ae0eda5b4dbd8c6fcd0db3
w <weasel@tutamail.com>
2020-03-24 12:05:38 +0100

    added name input

--- Project/src/main/java/de/wwweasel/SpringGettingStarted/SpringController.java
+++ Project/src/main/java/de/wwweasel/SpringGettingStarted/SpringController.java
@@ -1,7 +1,9 @@
 package de.wwweasel.SpringGettingStarted;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,7 +11,14 @@ public class SpringController {
 	
 	
 	@RequestMapping(method=RequestMethod.GET,value="/")
-	public String helloWorld() {
-		return "Hello World !";
+	public String helloWorld(@RequestParam(defaultValue="World") String name) {
+		String greeting = "Hello ";
+		return greeting + name;
+	}
+	
+	@RequestMapping(method=RequestMethod.GET,value="/{name}")
+	public String helloWorldPathVariable(@PathVariable("name") String name) {
+		String greeting = "Hello ";
+		return greeting + name;
 	}
 }
